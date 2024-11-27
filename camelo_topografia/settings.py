0e4d6a6544dce864db9e9d4ac10bbf794ceff175
EduCamelo <eduardocamelobs@gmail.com>
2024-11-27 19:05:44 -0300

    modelo do projeto

--- camelo_topografia/settings.py
+++ camelo_topografia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gksm713__#^hre!y!%%g5tt_vd%$4x$=knj0d$-j0*ivc=ee!=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
